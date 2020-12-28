0a1b5dd47e394379ecb4dc90c4de7971b4d3b124
vuvietvuong <anhvuonghn99@gmail.com>
2020-12-28 09:08:59 +0700

    change input matrix

--- matrix_inversion.py
+++ matrix_inversion.py
@@ -1,5 +1,5 @@
 import numpy as np
-matrix = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
+matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
 def matrix_inversion(matrix):
     matrix_invers = []
     a = matrix.shape
--- test_matrix_inversion.py
+++ test_matrix_inversion.py
@@ -9,8 +9,8 @@ class TestStringMethods(unittest.TestCase):
     expected_output = []
     # Setting up for the test
     def setUp(self):
-        matrix = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
-        expect = np.array([[10,9,8,7],[7,6,5,4],[4,3,2,1]])
+        matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+        expect = np.array([[9,8,7],[6,5,4],[3,2,1]])
         self.test_case.append(matrix)
         self.expected_output.append(expect)
     
